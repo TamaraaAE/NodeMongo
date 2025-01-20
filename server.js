e16b4f100c8aa0bdffbc66ecb9ef49b49e349846
TamaraaAE <tamarafiras@hotmail.com>
2025-01-20 10:40:56 +0100

    added express boilerplatecode

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Hello Tamara!");
+const express = require("express");
+const app = express();
+const PORT  = 3000;
+
+
+app.listen(PORT, () => {
+    console.log("listening to 3000");
+});
